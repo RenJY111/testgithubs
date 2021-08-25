6276445f1c30a4ea93ad37c6dbc1ef932dc903d9
qaz <782705802@qq.com>
2021-08-25 11:08:49 +0800

    继续开发代码

--- src/main/java/cn/lexed/git/HelloGits.java
+++ src/main/java/cn/lexed/git/HelloGits.java
@@ -1,5 +1,9 @@
 package cn.lexed.git;
 
 public class HelloGits {
-
+	public static void main(String[] args) {
+		System.out.println("git管理");
+		System.out.println("git管理12");
+		System.out.println("git管理23");
+	}
 }
